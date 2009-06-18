22e777fdf2348864a72663e42f3a7beb5ebbb6bb
jean-sebastien.guay@cm-labs.com <jean-sebastien.guay@cm-labs.com@297ef086-28dd-11de-a0a5-a9ea28672ce9>
2009-06-18 19:51:52 +0000

    - OceanScene::setOceanTechnique() did not check if the new ocean technique was valid before using it. It can be useful to set a null ocean technique when we want to disable its rendering.
    - OceanTechnique::start/stopAnimation() did not do anything and were not implemented in subclasses. Modified so that in the base class, they set a bool flag (and added a getter for that flag), and if subclasses want to do something more they can override them (calling the base class method to set the flag).
    
    - As a result of the above, FFTOceanSurface now checks the _isAnimating flag and if false, it doesn't update the normal mapping and the geometry. That way it's possible to stop the ocean surface animation and resume it.

--- trunk/src/osgOcean/OceanTechnique.cpp
+++ trunk/src/osgOcean/OceanTechnique.cpp
@@ -20,37 +20,27 @@
 using namespace osgOcean;
 
 OceanTechnique::OceanTechnique(void):
-	_isDirty( true )
+    _isDirty( true )
 {
 
 }
 
 OceanTechnique::OceanTechnique( const OceanTechnique& copy, const osg::CopyOp& copyop ):
-	osg::Geode		( copy, copyop ),
-	_isDirty			( true )
+    osg::Geode ( copy, copyop ),
+    _isDirty   ( true )
 {
 
 }
 
 void OceanTechnique::build(void)
 {
-	osg::notify(osg::DEBUG_INFO) << "OceanTechnique::build() Not Implemented" << std::endl;
-}
-
-void OceanTechnique::stopAnimation(void)
-{
-	osg::notify(osg::DEBUG_INFO) << "OceanTechnique::stopAnimation() Not Implemented" << std::endl;
-}
-
-void OceanTechnique::startAnimation(void)
-{
-	osg::notify(osg::DEBUG_INFO) << "OceanTechnique::startAnimation() Not Implemented" << std::endl;
+    osg::notify(osg::DEBUG_INFO) << "OceanTechnique::build() Not Implemented" << std::endl;
 }
 
 float OceanTechnique::getSurfaceHeight(void) const
 {
-	osg::notify(osg::DEBUG_INFO) << "OceanTechnique::getSurfaceHeight() Not Implemented" << std::endl;
-	return 0.f;
+    osg::notify(osg::DEBUG_INFO) << "OceanTechnique::getSurfaceHeight() Not Implemented" << std::endl;
+    return 0.f;
 }
 
 
