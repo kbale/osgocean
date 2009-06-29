d584a339eb7859ccb8c1a8762733c86319001535
jean-sebastien.guay@cm-labs.com <jean-sebastien.guay@cm-labs.com@297ef086-28dd-11de-a0a5-a9ea28672ce9>
2009-06-29 15:22:12 +0000

    - Added missing #pragma once on some headers - Added missing copyright on one header. - Added setter and getter for enabling/disabling the underwater distortion effect. - Added getter for the underwater attenuation. - Checking of various enabled states is now finer grained so that effects can be disabled and they will take minimal rendering time when they are. - Reading of shaders in ShaderManager will now use a local function because .vert and .frag extensions and automatic setting of shader type based on extension were added after OSG 2.6.

--- src/osgOcean/ShaderManager.cpp
+++ src/osgOcean/ShaderManager.cpp
@@ -17,8 +17,47 @@
 
 #include <osgOcean/ShaderManager>
 #include <osgDB/ReadFile>
+#include <osgDB/fileutils>
+#include <osg/Version>
 
 using namespace osgOcean;
+
+
+osg::Shader* readShader(const std::string& filename)
+{
+    // The .vert and .frag extensions were added to the GLSL plugin in OSG 
+    // 2.7.3, and the automatic setting of shader type depending on the 
+    // extension was added in OSG 2.9.1. The code below lets us use OSG
+    // 2.6 and still get the same behavior.
+#if OPENSCENEGRAPH_MAJOR_VERSION > 2 || \
+    (OPENSCENEGRAPH_MAJOR_VERSION == 2 && OPENSCENEGRAPH_MINOR_VERSION > 9) || \
+    (OPENSCENEGRAPH_MAJOR_VERSION == 2 && OPENSCENEGRAPH_MINOR_VERSION == 9 && OPENSCENEGRAPH_PATCH_VERSION >= 1)
+
+    // This will search the registry's file path.
+    return osgDB::readShaderFile(filename);
+
+#else
+
+    // Determine shader type from the extension
+    osg::Shader::Type type = osg::Shader::UNDEFINED;
+    if (filename.find("vert") == filename.length() - 4)
+        type = osg::Shader::VERTEX;
+    else if (filename.find("frag") == filename.length() - 4)
+        type = osg::Shader::FRAGMENT;
+    else
+        return 0;
+
+    // Find the shader file in the osgDB data path list.
+    std::string fullpath = osgDB::findDataFile(filename);
+    if (fullpath.empty())
+        return 0;
+
+    // Read the shader file.
+    osg::Shader* shader = osg::Shader::readShaderFile(type, fullpath);
+    return shader;
+
+#endif
+}
 
 ShaderManager::ShaderManager()
 {
@@ -56,14 +95,14 @@ osg::Program* ShaderManager::createProgram( const std::string& name,
 
     if (loadFromFiles)
     {
-        vShader = osgDB::readShaderFile(osg::Shader::VERTEX, vertexSrc);
+        vShader = readShader(vertexSrc);
         if (!vShader)
         {
             osg::notify(osg::WARN) << "Could not read shader from file " << vertexSrc << std::endl;
             return NULL;
         }
 
-        fShader = osgDB::readShaderFile(osg::Shader::FRAGMENT, fragmentSrc);
+        fShader = readShader(fragmentSrc);
         if (!fShader)
         {
             osg::notify(osg::WARN) << "Could not read shader from file " << fragmentSrc << std::endl;
@@ -72,11 +111,11 @@ osg::Program* ShaderManager::createProgram( const std::string& name,
     }
     else
     {
-        if (vertexSrc.empty() == false)
+        if (!vertexSrc.empty())
         {
             vShader = new osg::Shader( osg::Shader::VERTEX, vertexSrc );
         }
-        if (fragmentSrc.empty() == false)
+        if (!fragmentSrc.empty())
         {
             fShader = new osg::Shader( osg::Shader::FRAGMENT, fragmentSrc );
         }
@@ -106,7 +145,7 @@ std::string ShaderManager::buildGlobalDefinitionsList(const std::string& name)
 {
     std::string list;
 
-    if (name.empty() == false)
+    if (!name.empty())
     {
         list += "// " + name + "\n";
     }
