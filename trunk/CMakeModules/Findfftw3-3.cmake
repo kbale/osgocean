ad2f800ae98d09446913f0dc1be6f93b1c1b6ed7
jean-sebastien.guay@cm-labs.com <jean-sebastien.guay@cm-labs.com@297ef086-28dd-11de-a0a5-a9ea28672ce9>
2009-10-08 18:59:47 +0000

    - Added env var checking for FFTW as had been done for FFTSS. - Removed the RELEASE_ONLY_BUILD sections in other CMakeLists.txt files, they're not needed anymore.

--- trunk/CMakeModules/Findfftw3-3.cmake
+++ trunk/CMakeModules/Findfftw3-3.cmake
@@ -1,17 +1,19 @@
 # Find double-precision (double) version of FFTW3
 
 FIND_PATH(
-	FFTW3-3_INCLUDE_DIR
-	NAMES fftw3.h
-	/usr/local/include
-    /usr/include
+    FFTW3-3_INCLUDE_DIR
+    NAMES fftw3.h
+    HINTS $ENV{FFTW3_DIR}/include
+    PATHS /usr/local/include
+          /usr/include
 )
 
 FIND_LIBRARY(
-	FFTW3-3_LIBRARY
-	NAMES fftw3-3 libfftw3-3
-	/usr/local/lib
-    /usr/lib
+    FFTW3-3_LIBRARY
+    NAMES fftw3-3 libfftw3-3
+    HINTS $ENV{FFTW3_DIR}/lib
+    PATHS /usr/local/lib
+          /usr/lib
 )
 
 SET(FFTW3_FOUND "NO")
--- trunk/CMakeModules/Findfftw3f-3.cmake
+++ trunk/CMakeModules/Findfftw3f-3.cmake
@@ -1,15 +1,19 @@
+# Find single-precision (float) version of FFTW3
+
 FIND_PATH(
-	FFTW3F-3_INCLUDE_DIR
-	NAMES fftw3.h
-	/usr/local/include
-    /usr/include
+    FFTW3F-3_INCLUDE_DIR
+    NAMES fftw3.h
+    HINTS $ENV{FFTW3_DIR}/include
+    PATHS /usr/local/include
+          /usr/include
 )
 
 FIND_LIBRARY(
-	FFTW3F-3_LIBRARY
-	NAMES fftw3f libfftw3f-3
-	/usr/local/lib
-    /usr/lib
+    FFTW3F-3_LIBRARY
+    NAMES fftw3f libfftw3f-3
+    HINTS $ENV{FFTW3_DIR}/lib
+    PATHS /usr/local/lib
+          /usr/lib
 )
 
 SET(FFTW_FOUND "NO")
